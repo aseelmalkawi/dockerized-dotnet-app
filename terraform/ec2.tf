5f7cea62837336489ba5fccf1dd6aeebf460ba3f
Aseel <malkawiaseel@live.com>
2026-02-19 14:59:24 -0500

    fix ami

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "main" {
-  ami                    = data.aws_ami.amazon_linux_2023.id
+  ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
