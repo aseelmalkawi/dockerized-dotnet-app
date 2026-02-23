ed0c6ca64f36f3266265d9119e8965fdd4104a9b
aseelmalkawi <malkawiaseel@live.com>
2026-02-23 06:56:19 +0300

    ecr uri source

--- common/load_outputs.sh
+++ common/load_outputs.sh
@@ -9,4 +9,4 @@ cd terraform
 terraform init -backend-config=backend-config.hcl
 # echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
 echo "ECR_URI=$(terraform output -raw ecr_repository_url)" >> $GITHUB_ENV
-
+echo "${{ env.ECR_URI }}"
