2ca61b9a4ab5b2b5d51f02ec326ff38b8548ba7c
aseelmalkawi <malkawiaseel@live.com>
2026-02-23 06:07:40 +0300

    fix output names

--- common/load_outputs.sh
+++ common/load_outputs.sh
@@ -7,6 +7,6 @@
 #     echo "ECR_URI=$(terraform output -raw ecr_repository_name)" >> $GITHUB_ENV
 cd terraform
 terraform init -backend-config=backend-config.hcl
-echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
-echo "ECR_URI=$(terraform output -raw ecr_repository_name)" >> $GITHUB_ENV
+# echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
+echo "ECR_URI=$(terraform output -raw ecr_repository_url)" >> $GITHUB_ENV
 
