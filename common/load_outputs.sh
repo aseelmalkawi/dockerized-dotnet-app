1fe9759b763fc8f02815a4dc1f1ad31f91fd525d
aseelmalkawi <malkawiaseel@live.com>
2026-02-23 05:32:09 +0300

    moved to tf dir in the outputs script

--- common/load_outputs.sh
+++ common/load_outputs.sh
@@ -5,7 +5,7 @@
 # run: |
 #     echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
 #     echo "ECR_URI=$(terraform output -raw ecr_repository_name)" >> $GITHUB_ENV
-
+cd terraform
 terraform init -backend-config=backend-config.hcl
 echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
 echo "ECR_URI=$(terraform output -raw ecr_repository_name)" >> $GITHUB_ENV
