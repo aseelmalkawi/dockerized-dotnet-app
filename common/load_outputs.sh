53e265be0a95d17adb9ca2efeb88bd3169fdb6fb
aseelmalkawi <malkawiaseel@live.com>
2026-02-24 03:13:24 +0300

    deploy

--- common/load_outputs.sh
+++ common/load_outputs.sh
@@ -1,11 +1,5 @@
-# - name: Init Terraform
-#   run: terraform init -backend-config=backend-config.hcl
-
-# - name: Export outputs
-# run: |
-#     echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
-#     echo "ECR_URI=$(terraform output -raw ecr_repository_name)" >> $GITHUB_ENV
 cd terraform
 terraform init -backend-config=backend-config.hcl
 # echo "PROJECT_NAME=$(terraform output -raw project_name)" >> $GITHUB_ENV
 echo "ECR_URI=$(terraform output -raw ecr_repository_url)" >> $GITHUB_ENV
+echo "INSTANCE_ID=$(terraform output -raw ec2_instance_id)" >> $GITHUB_ENV
