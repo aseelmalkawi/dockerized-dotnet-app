eef6ca927c88199b094347fc2611aecb646c49af
aseelmalkawi <malkawiaseel@live.com>
2026-03-03 02:17:08 +0300

    add gh cli tot eh runner lambda

--- aws/lambda.py
+++ aws/lambda.py
@@ -85,6 +85,10 @@ sudo systemctl start docker
 # Allow ubuntu user to run docker without sudo
 sudo usermod -aG docker ubuntu
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update && sudo apt install gh -y
+
 # Install and start service
 ./svc.sh install ubuntu
 ./svc.sh start
